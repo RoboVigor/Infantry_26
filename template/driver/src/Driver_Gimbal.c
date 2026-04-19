2e9354641bfd91e9ec5118e0c2ee6214e0168d77
808Taco <ma18901911152@163.com>
2026-04-19 13:57:59 +0800

    fix error

--- template/driver/src/Driver_Gimbal.c
+++ template/driver/src/Driver_Gimbal.c
@@ -5,8 +5,4 @@ float getGimbalGravityTorque(GyroscopeData_Type * gyroData) {
     float pitchRad = gyroData->pitch * PI / 180.0f; // 转换为弧度
     gravityTorque = gimbalMass * 9.81f * baryCenterZ * (cosf(pitchRad)*baryCenterX + sinf(pitchRad)*baryCenterZ); // 计算重力矩
     return gravityTorque;
-<<<<<<< HEAD
 }  
-=======
-}
->>>>>>> b48ee85b7c1b2fbee508a1118717b58620d696f6
